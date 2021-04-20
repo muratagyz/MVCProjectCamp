2921e3e7e5fc81b39f1e6ae9e8dc1a02beed7935
Murat AĞYÜZ <mrt.agyzz.00@outlook.com>
2021-04-20 23:56:47 +0300

    20.04.2021

---
+++ DataAccessLayer/Abstract/ICategoryDal.cs
@@ -0,0 +1,21 @@
+﻿using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Abstract
+{
+    public interface ICategoryDal
+    {
+        //CRUD
+        List<Category> list();
+
+        void Insert(Category p);
+
+        void Update(Category p);
+
+        void Delete(Category p);
+    }
+}
