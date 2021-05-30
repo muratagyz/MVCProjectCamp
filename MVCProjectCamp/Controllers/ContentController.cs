742499b40b97c76ae27caa81f883a1c38160dd48
Murat AĞYÜZ <mrt.agyzz.00@outlook.com>
2021-05-30 13:56:42 +0300

    30.05.2021

---
+++ MVCProjectCamp/Controllers/ContentController.cs
@@ -0,0 +1,25 @@
+﻿using BussinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCProjectCamp.Controllers
+{
+    public class ContentController : Controller
+    {
+        // GET: Content
+        ContentManager cm = new ContentManager(new EfContentDal());
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult ContentByHeading(int id)
+        {
+            var contentvalues = cm.GetListByHeadingId(id);
+            return View(contentvalues);
+        }
+    }
+}
