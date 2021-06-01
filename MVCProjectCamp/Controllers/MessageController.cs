4acd75d41d77013590f16e04e7831cfb53b369ee
Murat AĞYÜZ <mrt.agyzz.00@outlook.com>
2021-06-01 13:56:57 +0300

    01.06.2021

---
+++ MVCProjectCamp/Controllers/MessageController.cs
@@ -0,0 +1,39 @@
+﻿using BussinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Services.Description;
+
+namespace MVCProjectCamp.Controllers
+{
+    public class MessageController : Controller
+    {
+        // GET: Message
+        MessageManager mm = new MessageManager(new EfMessageDal());
+        public ActionResult Inbox()
+        {
+            var messagelist = mm.GetListInbox();
+            return View(messagelist);
+        }
+        public ActionResult SendBox()
+        {
+            var messagelist = mm.GetListSendbox();
+            return View(messagelist);
+        }
+        [HttpGet]
+        public ActionResult NewMessage()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult NewMessage(Message p)
+        {
+            
+            return View();
+        }
+    }
+}
