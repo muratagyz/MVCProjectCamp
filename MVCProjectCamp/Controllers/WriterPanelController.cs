3d1740f186362a1217909c28665e9d5bf7972221
Murat AĞYÜZ <mrt.agyzz.00@outlook.com>
2021-06-20 23:02:03 +0300

    20.06.2021

---
+++ MVCProjectCamp/Controllers/WriterPanelController.cs
@@ -0,0 +1,26 @@
+﻿using BussinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCProjectCamp.Controllers
+{
+    public class WriterPanelController : Controller
+    {
+        HeadingManager hm = new HeadingManager(new EfHeadingDal());
+        // GET: WriterPanel
+        public ActionResult WriterProfile()
+        {
+            return View();
+        }
+
+        public ActionResult MyHeading()
+        {
+            var values = hm.GetListByWriter();
+            return View(values);
+        }
+    }
+}
